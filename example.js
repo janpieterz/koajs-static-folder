bb4b154af6461abaaf1e7066b22e69dd6dbfd479
Jan-Pieter Zoutewelle <janpieterzoutewelle@gmail.com>
2014-03-12 15:11:00 +0100

    Added all files to example main body.

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ app.use(serve('./assets'));
 
 app.use(function *(next){
     if('/' == this.path){
-        this.body = 'Try GET `/assets/style.css`';
+        this.body = 'Try GET one of the following: `/assets/style.css`,`/public/blabla.json`,`/public/style.css`';
     }
 });
 
